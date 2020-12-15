4ec76f042dbcb3b58720b7028634d8f5526f0af9
Alex Cho <acho20@stuy.edu>
2020-12-15 14:25:59 -0500

    made frame, not working

--- Sorts.java
+++ Sorts.java
@@ -4,6 +4,16 @@ public class Sorts{
   *@param data  the elements to be sorted.
   */
   public static void bubbleSort(int[] data){
-    boolean tripped = false;
+    int placeholder = 0;
+    for(int i = 0; i < data.length - 1; i++){
+      for(int j = 0; j < data.length - 1 - i; j++){
+        if(data[j] < data[j + 1]){
+          placeholder = data[j];
+          data[j] = data[j+1];
+          data[j + 1] = placeholder;
+        }
+      }
+    }
   }
+  
 }
