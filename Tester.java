eb68bc64165e131e24ac34ee24fc8fdf489c8571
Alex Cho <acho20@stuy.edu>
2020-12-16 13:01:04 -0500

    tester code for bubblesort

---
+++ Tester.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+import java.util.Arrays;
+
+public class Tester{
+  public static void main(String[]args){
+    Random rand = new Random();
+    int[] test = new int[Math.abs(rand.nextInt() % 100)];
+    for (int i = 0; i < test.length; i++){
+      test[i] = rand.nextInt();
+    }
+    int[] test2 = Arrays.copyOf(test, test.length);
+    Sorts.bubbleSort(test);
+    Arrays.sort(test2);
+    System.out.println(Arrays.equals(test, test2));
+  }
+
+}
