40821a7d2e84c26dd147312a10146300d6e38972
Alex Cho <acho20@stuy.edu>
2020-12-16 13:00:06 -0500

    fixed issue with bubblesort()

--- Sorts.java
+++ Sorts.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Sorts{
   /**Bubble sort of an int array.
   *Upon completion, the elements of the array will be in increasing order.
@@ -5,9 +7,9 @@ public class Sorts{
   */
   public static void bubbleSort(int[] data){
     int placeholder = 0;
-    for(int i = 0; i < data.length - 1; i++){
+    for(int i = 0; i < data.length -1 ; i++){
       for(int j = 0; j < data.length - 1 - i; j++){
-        if(data[j] < data[j + 1]){
+        if(data[j] > data[j + 1]){
           placeholder = data[j];
           data[j] = data[j+1];
           data[j + 1] = placeholder;
@@ -15,5 +17,5 @@ public class Sorts{
       }
     }
   }
-  
+
 }
